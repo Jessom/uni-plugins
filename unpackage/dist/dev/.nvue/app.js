b282131b10718fa74d12c1b87802f84dd55f1ad8
watasisama <jessoncheung@gmail.com>
2024-09-12 11:24:36 +0800

    删除unpackage文件夹

--- unpackage/dist/dev/.nvue/app.js
+++
@@ -1,8 +0,0 @@
-Promise.resolve("./pages/template/watermarkCamera.js").then((res) => {
-  res.length;
-});
-Promise.resolve("./pages/template/camera.js").then((res) => {
-  res.length;
-});
-Promise.resolve("./app.css.js").then(() => {
-});
